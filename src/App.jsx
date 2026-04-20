201dc844db7bc937ca2953ca4b9a3adfc8cc1b84
Anvarmirzo Xusanov <anvarmirzoxusanov354@gmail.com>
2026-04-21 03:27:13 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './index.css'
+import React from 'react'
+
+function App() {
+
+
+  return (
+    <div className="text-3xl">
+      Hello world
+    </div>
+  )
+
+
+}
+
+export default App
