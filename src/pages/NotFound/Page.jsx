65eca81be7dd3b99462c3d828631dbd9b99cf98b
Anvarmirzo Xusanov <anvarmirzoxusanov354@gmail.com>
2026-04-28 14:21:28 +0500

    Fix: NotFound component naming and export

---
+++ src/pages/NotFound/Page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Page = () => {
+  return (
+    <div>404 Not found</div>
+  );
+};
+
+export default Page;
--- src/pages/NotFound/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page= () => {
-  return (
-    <div>404 Not found</div>
-  )
-}
-
-export default page
